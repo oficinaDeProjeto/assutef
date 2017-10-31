c266eb971642d88868ad3d4be3413190e677deb6
aline <alinenichelle@hotmail.com>
2017-10-31 01:19:08 -0200

    modal produto

--- client/src/app/models/produto.ts
+++ client/src/app/models/produto.ts
@@ -1,5 +1,11 @@
+import { Categoria } from "./categoria";
+
 export class Produto {
     id:string;
     nome:string;
     descicao:string;
+    valor:string;
+    categoria:Categoria;
+    unidade:string;
+    estoque:string;
 }
