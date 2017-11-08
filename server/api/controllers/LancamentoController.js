1c678e3c5fd20e238fe8d0c776c7f55069d1735a
unknown <balbinottijr@outlook.com>
2017-11-08 17:48:38 -0200

    lançamentos funcionnado 100%

---
+++ server/api/controllers/LancamentoController.js
@@ -0,0 +1,11 @@
+/**
+ * LancamentoController
+ *
+ * @description :: Server-side logic for managing chequinhoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
