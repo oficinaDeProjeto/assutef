b3d79be06bbd6ede1d15c53992fd12116d48b730
Luccas Gabriel <luccas_gabriel_mt@hotmail.com>
2017-11-20 22:29:50 -0200

    Paginator Usuario god

--- client/src/app/views/usuario/usuario.component.ts
+++ client/src/app/views/usuario/usuario.component.ts
@@ -2,8 +2,9 @@ import { UsuarioService } from './../../services/usuario/usuario.service';
 import { ModalUsuarioComponent } from './modal/modal-user.component';
 import { Usuario } from './../../models/Usuario';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry, MatDialog, MatSnackBar } from "@angular/material";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconRegistry, MatDialog, MatSnackBar, MatPaginator } from "@angular/material";
+import { ConfirmDialogService } from '../../components/common/confirm-dialog/confirm-dialog.service';
 
 
 @Component({
@@ -14,7 +15,12 @@ import { MatIconRegistry, MatDialog, MatSnackBar } from "@angular/material";
 export class UsuarioComponent implements OnInit {
 	usuarios : Usuario[] = [];
 	filteredUsuarios: Usuario[] = [];
-	selectedUsuario = new Usuario();
+	finalUsers: Usuario[] = [];
+
+	
+
+
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 	
 
 	ngOnInit() {
@@ -26,7 +32,7 @@ export class UsuarioComponent implements OnInit {
 		this.usuarioService.getAll().subscribe(usuarios => {
 			this.usuarios = <Usuario[]>usuarios;
 			this.filteredUsuarios = Object.assign([], this.usuarios);
-			this.selectedUsuario = this.usuarios[0];
+			this.filterUsuario("");
 		}, err => {
 			console.log(err);
 		});
@@ -36,7 +42,8 @@ export class UsuarioComponent implements OnInit {
 	constructor(
 		private usuarioService: UsuarioService,
 		private dialog: MatDialog,
-		public snackBar: MatSnackBar
+		public snackBar: MatSnackBar,
+		public confirmDialogService: ConfirmDialogService
 	) {
 	}
 	
@@ -50,13 +57,30 @@ export class UsuarioComponent implements OnInit {
 		});
 	}
 
+	delete(usuario): void{
+		this.confirmDialogService.confirm(
+			'Confirmação',
+			`Você tem ceteza que deseja remover o usuario ${usuario.nome}?`)
+			.subscribe(res => {
+				if (res) {
+					this.usuarioService.delete(usuario.id).subscribe(u => {
+						this.openSnackBar("Removido com sucesso", "OK");
+						this.getAll();
+					}, err => {
+						this.openSnackBar("Não foi possível remover o associado", "OK");
+					})
+				}
+			});
+	}
+
 	openUser(usuario): void{
 		let dialogRef = this.dialog.open(ModalUsuarioComponent, {
 			data: usuario
 		});
 
 		dialogRef.afterClosed().subscribe(result => {
-			this.save(result);
+			if(typeof result !== 'undefined')
+				this.save(result);
 		});
 	}
 
@@ -72,9 +96,17 @@ export class UsuarioComponent implements OnInit {
 		} else {
 			this.filteredUsuarios = Object.assign([], this.usuarios).filter(
 				u => u.nome.toLowerCase().indexOf(query.toLowerCase()) > -1 || u.email.toLowerCase().indexOf(query.toLowerCase()) > -1
-			)
+			);
 		}
+		this.finalUsers = this.filteredUsuarios.slice(0, Math.min(this.filteredUsuarios.length, this.paginator.pageSize));
 	}
+
+	onPaginateChange(event):void{
+		let startIndex = event.pageIndex * event.pageSize;
+		let endIndex = Math.min(startIndex + this.paginator.pageSize, this.filteredUsuarios.length);
+		this.finalUsers = this.filteredUsuarios.slice(startIndex, endIndex);
+		
+	 }
 
 
 	
