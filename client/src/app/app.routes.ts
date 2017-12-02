0fc625bde82d3e096b0cb85fec22e41b77753c01
leozztto <leozztto@gmail.com>
2017-12-02 18:49:10 -0200

    ajuste da remoção do banco incluso novamente, e ajustes no css

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -19,6 +19,7 @@ import { TipoconveniadoComponent } from './views/tipoconveniado/tipoconveniado.c
 import { ImpressaoChequinhoComponent } from './views/chequinho/impressao/impressao-chequinho.component';
 import { VendaComponent } from './views/venda/venda.component';
 import { CarrinhoComponent } from './views/carrinho/carrinho.component';
+import { BancoComponent } from './views/banco/banco.component';
 
 
 const routes: Routes = [
@@ -83,6 +84,13 @@ const routes: Routes = [
                 component: ConveniadoComponent,
                 data: {
                     breadcrumb: "Conveniado"
+                }
+            },
+            {
+                path: 'banco',
+                component: BancoComponent,
+                data: {
+                    breadcrumb: "Banco"
                 }
             },
             {
